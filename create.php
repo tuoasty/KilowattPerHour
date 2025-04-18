ceff2db55c63ca14a48a5fd809ecfe1d9aab0e6d
tuoasty <tuoasty@gmail.com>
2025-04-18 21:59:37 +0700

    remove debug

--- create.php
+++ create.php
@@ -1,16 +1,6 @@
 <?php
   require_once "connection.php";
-
-  $query = "SELECT * FROM users;";
-    $result = $conn->query($query);
-    if($result->num_rows > 0){
-      while($row = $result->fetch_assoc()){
-        echo "id : ". $row["id"]. " - Name: ".$row["name"]. " - Password: ". $row["password"]. "<br>";
-      }
-    } else {
-      echo "0 results";
-    }
-
+  
   if(isset($_POST["submit"])){
     $name = $_POST["name"];
     $password = $_POST["password"];
